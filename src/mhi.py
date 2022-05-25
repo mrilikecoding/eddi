43c90c432f302cbd49548f9ab5755f9ccd51f12e
mrilikecoding <nathanrgreen@gmail.com>
2022-05-24 19:27:22 -0700

    stub in mhi input function

--- src/mhi.py
+++ src/mhi.py
@@ -1,7 +1,17 @@
 import cv2
 import numpy as np
 
+from src.pipeline_node import PipelineNode
 
-class MotionImager:
-    def __init__(self, input_device):
-        self.input_device = input_device
+
+class MotionHistoryImager(PipelineNode):
+    def __init__(self, min_max_dimensions):
+        self.input_joint_list = []
+        super().__init__(min_max_dimensions)
+
+    def process_input_device_values(self, input_object_instance):
+        try:
+            if not self.input_joint_list:
+                self.input_joint_list = input_object_instance.joint_list
+        except Exception as e:
+            print(f"Problem parsing input device data: {e}")
