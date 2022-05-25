9b506257e2dbf09239d26c6d11a2a718e2f59d85
mrilikecoding <nathanrgreen@gmail.com>
2022-05-24 19:43:42 -0700

    import opengl and stub in skel draw function

--- src/mhi.py
+++ src/mhi.py
@@ -1,6 +1,12 @@
 import cv2
 import numpy as np
 
+
+import OpenGL
+import OpenGL.GL
+import OpenGL.GLUT
+import OpenGL.GLU
+
 from src.pipeline_node import PipelineNode
 
 
@@ -13,5 +19,16 @@ class MotionHistoryImager(PipelineNode):
         try:
             if not self.input_joint_list:
                 self.input_joint_list = input_object_instance.joint_list
+
+            self.draw_skeleton(input_object_instance)
+
         except Exception as e:
             print(f"Problem parsing input device data: {e}")
+
+    def draw_skeleton(self, input_object_instance):
+        for person, attrs in input_object_instance.people.items():
+            for joint in self.input_joint_list:
+                x = attrs[joint]["x"]
+                y = attrs[joint]["y"]
+                z = attrs[joint]["z"]
+                x, y, z = self.normalize_3d_point(x, y, z)
