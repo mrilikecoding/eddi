23f7f5aed3873865ab7accde64f36c669cc30000
mrilikecoding <nathanrgreen@gmail.com>
2022-06-27 18:08:43 -0700

    stub in gesture comparer

---
+++ src/gesture_comparer.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class GestureComparer:
+    def __init__(self, gesture_sequence_library=[]):
+        self.gesture_sequence_library = gesture_sequence_library
+        self.weights = np.array([1 for g in gesture_sequence_library])/len(gesture_sequence_library)
+    
+    def update_gesture_library(self, gesture_sequence_library):
+        self.gesture_sequence_library = gesture_sequence_library
+    
+    def compute_similarity(self, gesture_sequence):
+        pass
+    
+    def update_gesture_weights(self):
+        pass
+
