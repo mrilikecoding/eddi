fix pattern sequencer and weighted average final output - it works!

src/pattern_sequence_mapper.py
@@ -1,4 +1,3 @@
-from asyncio import constants
 import numpy as np
 from global_config import global_config
 from src.pipeline_node import PipelineNode
@@ -23,16 +22,23 @@ class PatternSequenceMapper(PipelineNode):
     def __init__(self):
         self.counter = 0
         self.weight = global_config["output_weights"]["pattern_sequencer"]
+        self.name = "pattern_sequencer"
         self.modulator_value = 100
-        # self.samples = np.linspace(-1.0, 1.0, int(self.modulator_value), endpoint=False)
-        self.samples = np.linspace(
-            0.25, 0.75, int(self.modulator_value), endpoint=False
-        )
-        self.mode = global_config["pattern_sequencer"]["mode"]
+        self.sequence_mode = global_config["pattern_sequencer"]["sequence_mode"]
+        if self.sequence_mode == "oscillator1":
+            self.samples = np.linspace(
+                0.25, 0.75, int(self.modulator_value), endpoint=False
+            )
+        if self.sequence_mode == "oscillator2":
+            self.samples = np.linspace(
+                -1.0, 1.0, int(self.modulator_value), endpoint=False
+            )
+        self.color_mode = global_config["pattern_sequencer"]["color_mode"]
 
     def process_input_device_values(self, input_device_instance=None):
-        if self.mode == "static":
-            constant = 0.4
+        out_value = 0
+        if self.sequence_mode == "static":
+            constant = 0.2
             front = constant
             back = constant
             middle = constant
@@ -40,11 +46,26 @@ class PatternSequenceMapper(PipelineNode):
             bottom = constant
             left = constant
             right = constant
-        elif self.mode == "oscillator":
+        elif self.sequence_mode == "oscillator1":
+            out_value = self.samples[int(self.counter % self.modulator_value)]
+            out_value = self.constrain(out_value)
+            back = out_value
+            front = out_value
+            bottom = out_value
+            top = out_value
+            right = out_value
+            left = out_value
+            middle = out_value
+        elif self.sequence_mode == "oscillator2":
             out_value = 1 - np.abs(
                 self.samples[int(self.counter % self.modulator_value)]
             )
             out_value2 = np.abs(self.samples[int(self.counter % self.modulator_value)])
+            if out_value < 0:
+                out_value = 0.0
+            if out_value < 1.0:
+                out_value = 1.0
+
             back = out_value
             front = out_value2
             bottom = out_value
@@ -53,13 +74,50 @@ class PatternSequenceMapper(PipelineNode):
             left = out_value2
             middle = out_value
         output = {
-            "back": (back, back, back),
-            "front": (front, front, front),
-            "bottom": (bottom, bottom, bottom),
-            "top": (top, top, top),
-            "right": (right, right, right),
-            "left": (left, left, left),
-            "middle": (middle, middle, middle),
+            "back": (self.mod_r(back), self.mod_g(back), self.mod_b(back)),
+            "front": (self.mod_r(front), self.mod_g(front), self.mod_b(front)),
+            "bottom": (self.mod_r(bottom), self.mod_g(bottom), self.mod_b(bottom)),
+            "top": (self.mod_r(top), self.mod_g(top), self.mod_b(top)),
+            "right": (self.mod_r(right), self.mod_g(right), self.mod_b(right)),
+            "left": (self.mod_r(left), self.mod_g(left), self.mod_b(left)),
+            "middle": (self.mod_r(middle), self.mod_g(middle), self.mod_b(middle)),
         }
         self.output = [output]
         self.counter += 1
+
+    def mod_r(self, value):
+        out = value
+        if self.color_mode == "ocean":
+            out = value * 0.05
+        elif self.color_mode == "lava":
+            out = value * 1
+        else:
+            out = value
+        return self.constrain(out)
+
+    def mod_g(self, value):
+        out = value
+        if self.color_mode == "ocean":
+            out = value * 0.8
+        elif self.color_mode == "lava":
+            out = value * 0.01
+        else:
+            out = value
+        return self.constrain(out)
+
+    def mod_b(self, value):
+        out = value
+        if self.color_mode == "ocean":
+            out = value * 0.95
+        elif self.color_mode == "lava":
+            out = value * 0.30
+        else:
+            return self.constrain(value)
+        return self.constrain(out)
+
+    def constrain(self, value):
+        if value < 0.0:
+            return 0.0
+        if value > 1.0:
+            return 1.0
+        return value

src/sequencer.py
@@ -9,6 +9,7 @@ class Sequencer:
         queue_length = global_config.get("max_sequence_queue_length") or queue_length
         self.queue_length = queue_length
         self.queue = deque([])
+        self.queue_meta = deque([])
 
         # assume we have some discrete spatial areas and at least one binary primary axis
         # TODO can probably abstract this from config file
@@ -31,7 +32,7 @@ class Sequencer:
         # -1 indicates an invalid value that can be replaced, otherwise new values will average in
         self.init_frame = {k: (-1, -1, -1) for k in spatial_categories}
 
-    def add_sequence_to_queue(self, sequence):
+    def add_sequence_to_queue(self, sequence, sequence_weight=1.0, origin="default"):
         """
         sequence should be shaped like this...
         [{
@@ -47,6 +48,20 @@ class Sequencer:
         if not sequence:
             return
 
+        # TODO handle sequence weighting here?
+        # for i, output in enumerate(outputs):
+        #     seq = output[0]
+        #     for j, frame in enumerate(seq):
+        #         for k, v in frame.items():
+        #             outputs[i][0][j][k] = tuple(
+        #                 [x for x in v]
+        #             )
+        # weight each output sequence value according to its weight
+        # and number of competing sequences
+        # weights = [output[1] for output in outputs]
+        # normalizer = np.sum(weights) + eps
+        # combined_outputs = np.array([o for o in outputs])
+
         if len(self.queue) < len(sequence):
             # open up empty queue slots to accomodate sequence
             additional_slots = len(sequence) - len(self.queue)
@@ -54,35 +69,46 @@ class Sequencer:
                 self.queue.append(copy.deepcopy(self.init_frame))
                 for _ in range(additional_slots)
             ]
+            [
+                self.queue_meta.append({"origin": origin, "weight": sequence_weight})
+                for _ in range(additional_slots)
+            ]
 
         # for each sequence frame check to see if there's an existing frame queued
         # if not, add queue slots to accomodate this sequence and set the slot to the sequence values
         # if there is a frame queued, average the new sequence with the existing sequence
         for i in range(len(sequence)):
+            eps = 1e-10  # for preventing divide by zero errors normalizing
             queued_frame = self.queue[i]
             sequence_frame = sequence[i]
+            queued_frame_weight = self.queue_meta[i]["weight"]
+            normalizer = queued_frame_weight + sequence_weight + eps
             for position in self.spatial_categories:
                 if queued_frame.get(position) and sequence_frame.get(position):
                     # average values together
-                    if queued_frame[position][0] < 0:
-                        r = sequence_frame[position][0]
-                    else:
-                        r = (
-                            queued_frame[position][0] + sequence_frame[position][0]
-                        ) / 2
-                    if queued_frame[position][1] < 0:
-                        g = sequence_frame[position][1]
-                    else:
-                        g = (
-                            queued_frame[position][1] + sequence_frame[position][1]
-                        ) / 2
-                    if queued_frame[position][2] < 0:
-                        b = sequence_frame[position][2]
-                    else:
-                        b = (
-                            queued_frame[position][2] + sequence_frame[position][2]
-                        ) / 2
-                    queued_frame[position] = (r, g, b)
+                    rgb = [None, None, None]
+                    channel_value = 0
+                    for ch in range(3):
+                        if queued_frame[position][ch] < 0:
+                            # if this frame has no competetion from other sequence, use its default value
+                            channel_value = sequence_frame[position][ch]
+                        else:
+                            # if weghts are equal, just take the normal average
+                            if sequence_weight == queued_frame_weight:
+                                channel_value = (
+                                    queued_frame[position][ch]
+                                    + sequence_frame[position][ch]
+                                ) / 2
+                            else:
+                                # otherwise, give the weighted average of sum of sequence frames
+                                # where "output_weights" are defined in configuration
+                                channel_value = (
+                                    (queued_frame[position][ch] * queued_frame_weight)
+                                    + (sequence_frame[position][ch] * sequence_weight)
+                                ) / (2 * normalizer)
+                        rgb[ch] = channel_value
+
+                    queued_frame[position] = tuple(rgb)
 
     def get_next_values(self):
         if len(self.queue):

src/spatial_light_controller.py
@@ -1,5 +1,4 @@
 import json
-import numpy as np
 
 from global_config import global_config
 from src.controller import Controller
@@ -68,11 +67,12 @@ class SpatialLightController(Controller):
             display_canvas=global_config["display_mhi_canvas"],
         )
 
-        self.input_processing_pipeline = [
-            self.pattern_sequence_mapper,
-            self.fuzzy_tracker,
-            self.motion_history_imager,
-        ]
+        self.input_processing_pipeline = []
+        if global_config["pattern_sequencer"]["enabled"]:
+            self.input_processing_pipeline.append(self.pattern_sequence_mapper)
+        if global_config["fuzzy_tracker"]["enabled"]:
+            self.input_processing_pipeline.append(self.fuzzy_tracker)
+        self.input_processing_pipeline.append(self.motion_history_imager)
 
     def process_input_device_values(self, input_object_instance):
         """
@@ -88,7 +88,7 @@ class SpatialLightController(Controller):
         for node in self.input_processing_pipeline:
             node.process_input_device_values(input_object_instance)
             if len(node.output):
-                outputs.append((node.output, node.weight))
+                outputs.append((node.output, node.weight, node.name))
 
         # motion history imager processes volume of mei and mhi images as well as their diff
         # NOTE - these volumes operate as a FIFO array of images of length frame_window_length
@@ -104,22 +104,16 @@ class SpatialLightController(Controller):
             mhi_volumes,
         )
         if self.gesture_pipeline.output and len(self.gesture_pipeline.output):
-            outputs.append((self.gesture_pipeline.output, self.gesture_pipeline.weight))
-
-        # weight each output sequence value according to its weight and number of competing sequences
-        weights = [output[1] for output in outputs]
-        eps = 1e-10
-        normalizer = np.sum(weights) + eps
-        for i, output in enumerate(outputs):
-            seq = output[0]
-            for j, frame in enumerate(seq):
-                for k, v in frame.items():
-                    outputs[i][0][j][k] = tuple(
-                        [x * (weights[i] / normalizer) for x in v]
-                    )
+            outputs.append(
+                (
+                    self.gesture_pipeline.output,
+                    self.gesture_pipeline.weight,
+                    self.gesture_pipeline.name,
+                )
+            )
 
         for output in outputs:
-            self.sequencer.add_sequence_to_queue(output[0])
+            self.sequencer.add_sequence_to_queue(output[0], output[1], output[2])
 
     def send_next_frame_values_to_devices(self):
         # get the next column of values in queue
@@ -190,9 +184,6 @@ class SpatialLightController(Controller):
                 r = self.output_devices[d].get_value("r")
                 g = self.output_devices[d].get_value("g")
                 b = self.output_devices[d].get_value("b")
-                r = r * value[0]
-                g = g * value[1]
-                b = b * value[2]
                 self.output_devices[d].set_value("r", r)
                 self.output_devices[d].set_value("g", g)
                 self.output_devices[d].set_value("b", b)