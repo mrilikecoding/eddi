f0015cbb362826c3d64b82e4c4aab619b74f7265
mrilikecoding <nathanrgreen@gmail.com>
2022-07-30 17:17:16 -0700

    fix sequencing by stacking a output matrix

--- src/sequencer.py
+++ src/sequencer.py
@@ -51,14 +51,12 @@ class Sequencer:
         if not len(outputs):
             return
 
-        max_sequence_len = 0
-        num_outputs = len(outputs)
-        # TODO  normalize this weights array to sum to 1
         eps = 1e-10  # for preventing divide by zero errors normalizing
         sequence_weights = np.array([o["weight"] for o in outputs])
         sequence_weights = sequence_weights / (np.sum(sequence_weights) + eps)
         sequences = [o["sequence"] for o in outputs]
 
+        max_sequence_len = 0
         for seq in sequences:
             if len(seq) > max_sequence_len:
                 max_sequence_len = len(seq)
@@ -71,79 +69,49 @@ class Sequencer:
                 for _ in range(additional_slots)
             ]
 
+        # layer in each output sequence frame into a column of unmerged values
+        # for each queue position
+        # TODO something is making the values rather small still, but the sequencing is working
         unmerged_queue = []
+        unmerged_queue_frame_weights = []
         for i, _ in enumerate(self.queue):
             frame_layers = []
             frame_layer_weights = []
             for j, sequence in enumerate(sequences):
+                # if the incoming sequence overlaps with this queue position
                 if i < len(sequence):
+                    # add the sequence frame to this queue column
                     frame_layers.append(sequence[i])
+                    # add the corresponding sequence weight
                     frame_layer_weights.append(sequence_weights[j])
             unmerged_queue.append(frame_layers)
+            unmerged_queue_frame_weights.append(frame_layer_weights)
 
         for i, frame_layers in enumerate(unmerged_queue):
+            r, g, b = (None, None, None)
             for position in self.spatial_categories:
                 r = self.queue[i][position][0]
                 g = self.queue[i][position][1]
                 b = self.queue[i][position][2]
+                # print(r, g, b)
                 for j, sequence_frame in enumerate(frame_layers):
                     if position in sequence_frame:
                         weighted_r = sequence_frame[position][0] * sequence_weights[j]
                         weighted_g = sequence_frame[position][1] * sequence_weights[j]
                         weighted_b = sequence_frame[position][2] * sequence_weights[j]
-                        r += weighted_r
-                        g += weighted_g
-                        b += weighted_b
-            self.queue[i][position] = (r, g, b)
+                        if all([ch >= 0 for ch in [r, g, b]]):
+                            r += weighted_r
+                            g += weighted_g
+                            b += weighted_b
+                        else:
+                            r = weighted_r
+                            g = weighted_g
+                            b = weighted_b
+                self.queue[i][position] = (r, g, b)
         self.director.set_current_queue(
             copy.copy(self.queue), copy.copy(self.queue_meta)
         )
         return
-        # [
-        #     self.queue_meta.append({"origin": origin, "weight": sequence_weight})
-        #     for _ in range(additional_slots)
-        # ]
-
-        # for each sequence frame check to see if there's an existing frame queued
-        # if not, add queue slots to accomodate this sequence and set the slot to the sequence values
-        # if there is a frame queued, average the new sequence with the existing sequence
-        for i in range(len(sequence)):
-            eps = 1e-10  # for preventing divide by zero errors normalizing
-            queued_frame = self.queue[i]
-            sequence_frame = sequence[i]
-            queued_frame_weight = self.queue_meta[i]["weight"]
-            normalizer = queued_frame_weight + sequence_weight + eps
-            for position in self.spatial_categories:
-                if queued_frame.get(position) and sequence_frame.get(position):
-                    # average values together
-                    rgb = [None, None, None]
-                    channel_value = 0
-                    for ch in range(3):
-                        if queued_frame[position][ch] < 0:
-                            # if this frame has no competetion from other sequence, use its default value
-                            channel_value = sequence_frame[position][ch]
-                        else:
-                            # if weghts are equal, just take the normal average
-                            if sequence_weight == queued_frame_weight:
-                                channel_value = (
-                                    queued_frame[position][ch]
-                                    + sequence_frame[position][ch]
-                                ) / 2
-                            else:
-                                # otherwise, give the weighted average of sum of sequence frames
-                                # where "output_weights" are defined in configuration
-                                channel_value = (
-                                    (queued_frame[position][ch] * queued_frame_weight)
-                                    + (sequence_frame[position][ch] * sequence_weight)
-                                ) / (2 * normalizer)
-                        rgb[ch] = channel_value
-
-                    queued_frame[position] = tuple(rgb)
-        # send the latest queued output to the Director class instance for
-        # final stage transformations
-        self.director.set_current_queue(
-            copy.copy(self.queue), copy.copy(self.queue_meta)
-        )
 
     def get_next_values(self):
         queue = copy.copy(self.director.get_current_queue())
